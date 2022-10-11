66e70d346c98b665d9e651fce692f38f6f8e6f1c
andersonprovox <andersongfonseca@gmail.com>
2022-10-11 18:18:06 -0300

    vinculando a tabela de aulas com professores

--- backend/app/views.py
+++ backend/app/views.py
@@ -2,5 +2,5 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class HomeApiView(APIView):
-    def get(self, request, format=none):
+    def get(self, request, format=None):
         return Response({"nome": "Anderson Fonseca", "idade": 37 }, status=200)
